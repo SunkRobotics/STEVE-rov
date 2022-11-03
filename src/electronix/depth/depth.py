9c47b65139cc6153f4b191ed9087d7862bfe9ae0
Sunk Robotics <sunkrobotics@gmail.com>
2022-11-03 17:01:00 -0400

    Depth sensor code + removed unnecessary libraries

---
+++ src/electronix/depth/depth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import ms5837
+import time
+
+sensor = ms5837.MS5837_30BA() # Default I2C bus is 1 (Raspberry Pi 3)
+#sensor = ms5837.MS5837_30BA(0) # Specify I2C bus
+#sensor = ms5837.MS5837_02BA()
+#sensor = ms5837.MS5837_02BA(0)
+#sensor = ms5837.MS5837(model=ms5837.MS5837_MODEL_30BA, bus=0) # Specify model and bus
+
+# We must initialize the sensor before reading it
+if not sensor.init():
+        print("Sensor could not be initialized")
+        exit(1)
+
+# We have to read values from sensor to update pressure and temperature
+if not sensor.read():
+    print("Sensor read failed!")
+    exit(1)
+
+# Spew readings
+while True:
+        if sensor.read():
+                print("P: %0.1f mbar  %0.3f psi\tT: %0.2f C  %0.2f F") % (
+                sensor.pressure(), # Default is mbar (no arguments)
+                sensor.pressure(ms5837.UNITS_psi), # Request psi
+                sensor.temperature(), # Default is degrees C (no arguments)
+                sensor.temperature(ms5837.UNITS_Farenheit)) # Request Farenheit
+        else:
+                print("Sensor read failed!")
+                exit(1)
--- src/motors/motor_test.py
+++ src/motors/motor_test.py
@@ -1,12 +1,5 @@
 #  import adafruit_motor.servo
 from adafruit_servokit import ServoKit
-import adafruit_pca9685
-import board
-import busio
-import time
-
-i2c = busio.I2C(board.SCL, board.SDA)
-pca = adafruit_pca9685.PCA9685(i2c)
 
 kit = ServoKit(channels=16, reference_clock_speed=26541466)
 
@@ -18,3 +11,4 @@ for servo_num in range(6):
 kit.servo[0].set_pulse_width_range(1100, 1900)
 kit.servo[0].angle = 90
 kit.servo[1].angle = 90
+kit.servo[2].angle = 90
