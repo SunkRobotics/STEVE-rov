62fee56975c9b0bdad4bb77a3063fdeadfc6b2ba
XiXinping <81447868+XiXinping@users.noreply.github.com>
2023-03-01 13:27:56 -0500

    Changed IP Address

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -5,15 +5,16 @@ let greetMsgEl: HTMLElement | null;
 
 const feed1 = document.querySelector('#feed1') as HTMLCanvasElement;
 // const feed2 = document.querySelector('#feed2') as HTMLImageElement;
-let ws_feed1 = new WebSocket("ws://192.168.0.102:3000");
+let ws_feed1 = new WebSocket("ws://192.168.1.150:3000");
 ws_feed1.addEventListener('message', (event: MessageEvent) => {
   event.data.text().then((res: string) => {
     var img = new window.Image();
     img.setAttribute("src", "data:image/jpg;base64," + res);
 
     // let feed_size = feed1.getBoundingClientRect();
+    const feed1_ctx = feed1.getContext("2d")!;
     img.addEventListener("load", function () {
-        feed1.getContext("2d")!.drawImage(img, 0, 0, img.width, img.height, 0, 0, feed1.width, feed1.height);
+        feed1_ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, feed1.width, feed1.height);
     });
   })
 })
