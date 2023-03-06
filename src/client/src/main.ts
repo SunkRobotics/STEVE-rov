c0741bc8c6e520d86abf0a8e3316e87192680b2d
XiXinping <81447868+XiXinping@users.noreply.github.com>
2023-03-06 13:53:21 -0500

    Starting to work on game controller code

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -1,38 +1,37 @@
-import { invoke } from "@tauri-apps/api/tauri";
-
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
 const feed1 = document.querySelector('#feed1') as HTMLCanvasElement;
 // const feed2 = document.querySelector('#feed2') as HTMLImageElement;
-let ws_feed1 = new WebSocket("ws://192.168.1.150:3000");
+let ws_feed1 = new WebSocket("ws://192.168.0.102:3000");
+
 ws_feed1.addEventListener('message', (event: MessageEvent) => {
   event.data.text().then((res: string) => {
-    var img = new window.Image();
+    var img = new Image();
     img.setAttribute("src", "data:image/jpg;base64," + res);
 
-    // let feed_size = feed1.getBoundingClientRect();
     const feed1_ctx = feed1.getContext("2d")!;
     img.addEventListener("load", function () {
-        feed1_ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, feed1.width, feed1.height);
+        feed1_ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 1280, 720);
     });
   })
 })
+ws_feed1.addEventListener('close', (_) => {
+  let img = new Image();
+  img.setAttribute("src", "/src/assets/static.gif");
+  const feed1_ctx = feed1.getContext("2d")!;
+  feed1_ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 1280, 720);
+})
 
+ws_feed1.addEventListener('error', (_) => {
+  let img = new Image();
+  img.setAttribute("src", "/src/assets/static.gif");
+  const feed1_ctx = feed1.getContext("2d")!;
+  feed1_ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 1280, 720);
+})
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
-}
+let gamepad: Gamepad | null;
+window.addEventListener("gamepadconnected", (event) => {
+  gamepad = event.gamepad;
+});
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    ?.addEventListener("click", () => greet());
+window.addEventListener("gamepaddisconnected", (_) => {
+  gamepad = null;
 });
