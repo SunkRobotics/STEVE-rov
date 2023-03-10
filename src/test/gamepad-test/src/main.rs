3882c52d386bc72546db7b0d538662aa3a0fbf7d
XiXinping <81447868+XiXinping@users.noreply.github.com>
2023-03-10 13:20:00 -0500

    Added dead zone filtering and code for connecting to RPi

--- src/test/gamepad-test/src/main.rs
+++ src/test/gamepad-test/src/main.rs
@@ -1,10 +1,15 @@
 #[allow(dead_code)]
+extern crate serde_json;
 use anyhow::Result;
+use early_returns::some_or_return;
+use gilrs::{Axis, Button, Gamepad, GamepadId, GilrsBuilder, Gilrs};
 use serde::Serialize;
 use std::collections::HashMap;
 use std::time::Duration;
 use std::thread;
-use gilrs::{Axis, Button, GamepadId, Gilrs};
+use tungstenite::{Message};
+use url::Url;
+// use gilrs::ev::filter::{Jitter, deadzone};
 
 #[derive(Debug, Serialize)]
 struct JoystickValues {
@@ -25,15 +30,37 @@ impl JoystickValues {
     } 
 }
 
+fn get_filtered_axis(gamepad: Gamepad, axis: Axis) -> f32 {
+    let axis_code = some_or_return!(gamepad.axis_code(axis), 0.0);
+    let deadzone = some_or_return!(gamepad.deadzone(axis_code), 0.0);
+    let axis_value = gamepad.value(axis);
+
+    if axis_value.abs() < deadzone {
+        return 0.0;
+    } else {
+        return axis_value;
+    }
+}
 
 fn main() -> Result<()> {
     let mut gilrs: Gilrs;
+    let mut websocket = None;
     let gamepad_id: GamepadId;
 
+    loop {
+        if let Ok((mut socket, _)) = tungstenite::connect(Url::parse("ws://localhost:8765")?) {
+            websocket = Some(socket);
+            break;
+        } else {
+            thread::sleep(Duration::from_millis(1));
+            continue;
+        };
+
+    }
     // wait for the gamepad to connect
     println!("Waiting for gamepad to connect!");
     loop {
-        gilrs = Gilrs::new().unwrap();
+        gilrs = GilrsBuilder::new().with_default_filters(false).build().unwrap();
         if let Some((_id, gp)) = gilrs.gamepads().next() {
             gamepad_id = gp.id();
             break;
@@ -61,8 +88,8 @@ fn main() -> Result<()> {
         buttons.insert("Select".to_string(), gamepad.is_pressed(Button::Select));
 
 
-        let left_stick = (gamepad.value(Axis::LeftStickX), gamepad.value(Axis::LeftStickY));
-        let right_stick = (gamepad.value(Axis::RightStickX), gamepad.value(Axis::RightStickY));
+        let left_stick = (get_filtered_axis(gamepad, Axis::LeftStickX), get_filtered_axis(gamepad, Axis::LeftStickY));
+        let right_stick = (get_filtered_axis(gamepad, Axis::RightStickX), get_filtered_axis(gamepad, Axis::RightStickY));
 
         let dpad_x = if gamepad.is_pressed(Button::DPadLeft) {
             -1
@@ -82,7 +109,10 @@ fn main() -> Result<()> {
         let dpad = (dpad_x, dpad_y);
 
         let joystick_values = JoystickValues::new(buttons, left_stick, right_stick, dpad);
-        println!("{:?}", joystick_values);
+
+        
+        println!("Connected to ROV!");
+        // websocket.as_ref().unwrap().write_message(Message::Text(serde_json::to_string(&joystick_values)?))?;
 
         thread::sleep(Duration::from_millis(1));
     }
