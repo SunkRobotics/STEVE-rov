79573ce0fe6e2fa96e4b253b56477671ce978499
Random User <about@example.com>
2023-04-20 18:38:41 -0400

    Added coral task stuff to client

--- src/new-client/src/main.js
+++ src/new-client/src/main.js
@@ -1,7 +1,7 @@
 let cam1 = document.getElementById("cam1");
 let cam2 = document.getElementById("cam2");
 
-let metric = 1;
+let metric = 2;
 let pixPerMetric = 0;
 let points = []
 
@@ -13,16 +13,17 @@ function takePicture() {
     let info = document.createElement("p")
     let pic = document.createElement("img")
 
-    pic.src = "countries.jpg"
+    pic.src = "coral.jpg"
 
     pic.addEventListener("click", (e) => {
         points.push([e.offsetX, e.offsetY])
+        console.log(points)
 
         if (points.length == 2) {
             let difX = points[0][0] - points[1][0]
             let difY = points[0][1] - points[1][1]
 
-            let distance = Math.sqrt(difX**difX + difY**difY)
+            let distance = Math.sqrt(difX*difX + difY*difY)
             console.log(distance)
 
             pixPerMetric = distance / metric
@@ -32,13 +33,26 @@ function takePicture() {
             let difX = points[2][0] - points[3][0]
             let difY = points[2][1] - points[3][1]
 
-            let pixDistance = Math.sqrt(difX**difX + difY**difY)
+            let pixDistance = Math.sqrt(difX*difX + difY*difY)
 
             let distance = pixDistance / pixPerMetric
 
+            console.log(distance + " in.")
             new_space.querySelector("p").innerHTML = `${distance}`
 
-            points = []
+        }
+        if (points.length == 6) {
+            let difX = points[4][0] - points[5][0]
+            let difY = points[4][1] - points[5][1]
+
+            let pixDistance = Math.sqrt(difX*difX + difY*difY)
+
+            const MAGIC_CONSTANT = 1.25
+            let distance = pixDistance / pixPerMetric * MAGIC_CONSTANT
+
+            console.log(distance + " in.")
+            new_space.querySelector("p").innerHTML = `${distance}`
+
         }
     })
 
