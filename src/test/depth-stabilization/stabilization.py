565d70bdbf3fb59cdcd1c3817e6d87cc9ff119f6
XiXinping <81447868+XiXinping@users.noreply.github.com>
2023-05-03 22:35:17 -0400

    Depth stabilization prototype (needs testing)

--- src/test/depth-stabilization/stabilization.py
+++ src/test/depth-stabilization/stabilization.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
-import motors.py
+from motors.py import Motors
 import ms5837
 import time
+
 
 class PID:
     last_time = None
     last_error = None
     error_sum = 0
 
-    def __init__(self, set_point, proportional_gain, integral_gain,
-                 derivative_gain):
+    def __init__(self, set_point: float, proportional_gain: float,
+                 integral_gain: float, derivative_gain: float):
         self.set_point = set_point
         self.proportional_gain = proportional_gain
         self.integral_gain = integral_gain
@@ -18,7 +19,7 @@ class PID:
         self.last_time = time.time()
         self.last_error = set_point
 
-    def compute(self, process_value):
+    def compute(self, process_value: float) -> float:
         current_time = time.time()
         d_time = time.time() - self.last_time
         print(f"Change in Time: {d_time}")
@@ -39,6 +40,12 @@ class PID:
 
         output = (self.proportional_gain * error + self.integral_gain
                   * self.error_sum + self.derivative_gain * d_error)
+
+        # make sure output within the valid motors input range
+        if output > 1:
+            output = 1
+        elif output < 0:
+            output = 0
         print(f"Output: {output}")
         return output
 
@@ -55,6 +62,8 @@ class Plant:
         return self.plant_state
 
 
+motors = Motors()
+
 depth_sensor = ms5837.MS5837_02BA(1)
 
 # initialize the sensor before reading it
@@ -62,7 +71,24 @@ if not depth_sensor.init():
     print("Depth sensor could not be initialized")
     exit(1)
 
-pid_controller = PID(0.5, 0.5, 3, 0.004)
+target_depth = 0.5  # 0.5 meters
+proportional_gain = 0.1
+integral_gain = 3
+derivative_gain = 0.004
+pid_controller = PID(target_depth, proportional_gain, integral_gain,
+                     derivative_gain)
 
+while True:
+    if not depth_sensor.read():
+        time.sleep(0.01)
+        continue
+
+    current_depth = depth_sensor.depth()
+    # use the PID controller to determine the best z velocity based on the
+    # current depth
+    new_z_velocity = pid_controller.compute(current_depth)
+    motors.drive_motors(z_velocity=new_z_velocity)
+
+    time.sleep(0.01)
 
 
