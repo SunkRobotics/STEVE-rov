d16d949043313542fc8d1e06565f516e879270e0
Random User <about@example.com>
2023-06-06 17:27:17 -0400

    Image metadata stuff

--- src/camera/camera_metadata.py
+++ src/camera/camera_metadata.py
@@ -9,26 +9,29 @@ from websockets import serve, ConnectionClosed
 
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
+        self.metadata = None
         self.frame = None
+        self.picam2 = None
 
     def write(self, buf):
         self.frame = buf
+        if self.picam2:
+            self.metadata = self.picam2.capture_metadata()
 
 
 class WSServer:
     output = StreamingOutput()
 
     @classmethod
-    async def handler(cls, websocket, path):
+    async def handler(cls, websocket, path, output):
         print("Client connected!")
         try:
             while True:
                 if cls.output.frame is None:
                     await asyncio.sleep(0.001)
                     continue
-                # str_data = base64.b64encode(cls.output.frame)
 
-                await websocket.send(bytearray(cls.output.frame))
+                await websocket.send(bytearray(cls.output.frame), bytearray(cls.output.metadata))
                 await asyncio.sleep(0.001)
 
         except ConnectionClosed:
@@ -37,10 +40,12 @@ class WSServer:
 
 def main():
     picam2 = Picamera2()
+    WSServer.output.picam2 = picam2
     picam2.set_controls({"AfMode": 2})
     picam2.configure(picam2.create_video_configuration(
         main={"size": (1920, 1080)}))
-    picam2.start_recording(MJPEGEncoder(50_000_000), FileOutput(WSServer.output))
+    picam2.start_recording(MJPEGEncoder(50_000_000),
+                           FileOutput(WSServer.output))
 
     loop = asyncio.get_event_loop()
     ws_server = serve(
@@ -52,4 +57,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
